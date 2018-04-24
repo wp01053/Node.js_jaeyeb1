84d5274dbf5ed012a82a4df8249c45d67487cc4f
jaeyeoupjung <wp01053@naver.com>
2018-04-24 12:25:24 +0900

    first commit

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const http = require('http');
-let server = http.createServer(function (req, res){
-res.writeHead(200,{'Content-type':'text/html'});
-res.write('Hello, world!');
-res.end();
-});
+function onRequest(req,res){
+    res.writeHead(200,{'Content-type':'text/html'});
+    res.write('Hello, world!');
+    res.end();
+}
+
+let server = http.createServer(onRequest);
 server.listen(8000, 'localhost');
 console.log('Server is running at localhost:8000');

981c647ae26a883cf9eddf2beae2a33c1fa99507
jaeyeoupjung <wp01053@naver.com>
2018-04-24 12:21:29 +0900

    first commit

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-console.log('Node.js version : ' + process.version);
-console.log(process.memoryUsage());
-console.log(process.env);
-//console.log(module);
-
 const circle = require('./circle');
 console.log('Area of ac circle =' + circle.area(3)); //모듈을 사용한것
 console.log('Circumference of ac circle =' + circle.circum(3));
 console.log('Pi =' + circle.pi);
+
+const square = require('./square');
+let mysquare = square(4);
+console.log('Area of a square = '+ mysquare.area());

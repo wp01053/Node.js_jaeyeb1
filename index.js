411962bf72634c228e831a54aa47b48070c72f10
jaeyeoupjung <wp01053@naver.com>
2018-04-24 12:29:29 +0900

    first commit

--- index.js
+++ index.js
@@ -2,5 +2,11 @@ let nport=8000;
 let sHost='localhost'
 const myServer = require('./myServer');
 const myRouter = require('./myrouter');
+const myHandlers = require('./myhandlers');
 
-myServer.start(nport, sHost, myRouter.route);
+let handle = {};
+handle['/'] = myHandlers.start;
+handle['/start'] = myHandlers.start;
+handle['/hello'] = myHandlers.hello;
+
+myServer.start(nport, sHost, myRouter.route, handle);

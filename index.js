1d89678f6d05ff686aef0eac6e1993b97bf09312
jaeyeoupjung <wp01053@naver.com>
2018-04-24 12:24:22 +0900

    first commit

--- index.js
+++ index.js
@@ -1,14 +1,8 @@
-const circle = require('./circle');
-console.log('Area of ac circle =' + circle.area(3)); //모듈을 사용한것
-console.log('Circumference of ac circle =' + circle.circum(3));
-console.log('Pi =' + circle.pi);
-
-const square = require('./square');
-let mysquare = square(5);
-mysquare.width = 10;
-console.log('Area of a square = '+ mysquare.area());
-console.log('Circumference of ac square = '+ mysquare.circum());
-
-const os = require('os');
-console.log(os.type());
-console.log(os.cpus());
+const http = require('http');
+let server = http.createServer(function (req, res){
+res.writeHead(200,{'Content-type':'text/html'});
+res.write('Hello, world!');
+res.end();
+});
+server.listen(8000, 'localhost');
+console.log('Server is running at localhost:8000');

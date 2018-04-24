f284d6cb29726ea7eb5e571d853b7ba1aa6d8f38
jaeyeoupjung <wp01053@naver.com>
2018-04-24 12:22:20 +0900

    first commit

--- index.js
+++ index.js
@@ -4,5 +4,11 @@ console.log('Circumference of ac circle =' + circle.circum(3));
 console.log('Pi =' + circle.pi);
 
 const square = require('./square');
-let mysquare = square(4);
+let mysquare = square(5);
+mysquare.width = 10;
 console.log('Area of a square = '+ mysquare.area());
+console.log('Circumference of ac square = '+ mysquare.circum());
+
+const os = require('os');
+console.log(os.type());
+console.log(os.cpus());

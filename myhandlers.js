a32862951d29812d4013211d8553d8bf9bcb0d8d
jaeyeoupjung <wp01053@naver.com>
2018-05-01 11:24:03 +0900

    18-05-01 cloud

--- myhandlers.js
+++ myhandlers.js
@@ -1,13 +1,22 @@
+function sleep(milliSec){
+    let timeStart = new Date().getTime();
+    while (new Date().getTime() < timeStart + milliSec);
+}
+
 function start(){
     res.writeHead(200, {'Content-Type' :'texts/html' });
     res.write('Hello, start!');
     res.end();    
 }
-function hello(){
+function hello(res){
+   // sleep(20000);
+   setTimeout(function(){
+    
     res.writeHead(200, {'Content-Type' :'texts/html' });
     res.write('Hello, hello!');
     res.end();    
+   }, 20000);
 }
 
-exports.start =start;
+exports.start = start;
 exports.hello = hello;
